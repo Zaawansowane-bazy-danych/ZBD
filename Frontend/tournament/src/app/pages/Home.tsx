cf13e58c4abcc85966331ff2e3ff1c3c54996923
FrasMaciej <maciejfrs@gmail.com>
2024-06-01 13:04:13 +0200

    Ant error fix + basic fetch test

--- Frontend/tournament/src/app/pages/Home.tsx
+++ Frontend/tournament/src/app/pages/Home.tsx
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { API_URL } from '../../config';
 
 function Home() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(API_URL || '');
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
-        <></>
+        <div>
+        </div>
     );
 }
 
--- Frontend/tournament/src/app/pages/Layout.tsx
+++ Frontend/tournament/src/app/pages/Layout.tsx
@@ -3,26 +3,36 @@ import { Menu } from "antd";
 import { HomeOutlined, TrophyOutlined } from "@ant-design/icons";
 
 const Layout = () => {
+  const menuItems = [
+    {
+      key: "1",
+      label: (
+        <Link to="/">Welcome</Link>
+      )
+    },
+    {
+      key: "2",
+      label: (
+        <Link to="/home">
+          <HomeOutlined />
+          <span style={{ marginLeft: "4px" }}>Home</span>
+        </Link>
+      )
+    },
+    {
+      key: "3",
+      label: (
+        <Link to="/tournament">
+          <TrophyOutlined />
+          <span style={{ marginLeft: "4px" }}>Tournament</span>
+        </Link>
+      )
+    }
+  ];
+
   return (
     <>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1">
-          <Link to="/">Welcome</Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link to="/home">
-            <HomeOutlined />
-            <span style={{ marginLeft: "4px" }}>Home</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Link to="/tournament">
-            <TrophyOutlined />
-            <span style={{ marginLeft: "4px" }}>Tournament</span>
-          </Link>
-        </Menu.Item>
-      </Menu>
-
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]} items={menuItems} />
       <Outlet />
     </>
   );
